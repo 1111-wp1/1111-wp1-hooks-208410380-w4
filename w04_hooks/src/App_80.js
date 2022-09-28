851c2c9f26f76bb61fdad66d5ead5d118b6dcaa1
justin40715 <justin40715@gmail.com>
2022-09-28 19:35:32 +0800

    Hooks Tutorial T11_80

---
+++ w04_hooks/src/App_80.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T11_80 from './tutorial/1-useState/T11_80_error-example';
+
+const App_80 = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial 11 - 123456789</h2>
+      <T11_80 />
+    </div>
+  );
+};
+
+export default App_80;
