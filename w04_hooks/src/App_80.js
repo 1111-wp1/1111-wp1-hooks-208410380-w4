82d4e6a5f15bc1ee23b3ac125d9881e57e80e980
justin40715 <justin40715@gmail.com>
2022-09-28 19:57:42 +0800

    Hooks Tutorial T12_80, fix problem in T12_80

--- w04_hooks/src/App_80.js
+++ w04_hooks/src/App_80.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T11_80 from './tutorial/1-useState/T11_80_error-example';
+import T12_80 from './tutorial/1-useState/T12_80_error-example';
 
 const App_80 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 11 - 123456789</h2>
-      <T11_80 />
+      <h2>Tutorial 12 - 123456789</h2>
+      <T12_80 />
     </div>
   );
 };
